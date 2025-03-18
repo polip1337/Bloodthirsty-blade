175fa23def9cc4d26bcad16f5477b078c9f7ee53
konra <konrad.czamanski@gmail.com>
2025-03-18 16:28:42 +0100

    Ui improvements

--- js/main.js
+++ js/main.js
@@ -8,35 +8,7 @@ async function initGame() {
     saveGame();
     calculateMaxEnergy();
     updateDisplay();
-    setInterval(() => {
-        if (game.wielder.currentLife < game.wielder.currentStats.endurance * 5) {
-                let regenRate = 1; // Base regen
-                if (game.currentAction === 'resting') {
-                    regenRate += 4; // Resting bonus
-                }
-                game.wielder.currentLife = Math.min(
-                    game.wielder.currentLife + regenRate,
-                    getEffectiveStats().endurance * 5
-                );
-                updateHealthBar();
-                console.log(`Health regenerated: +${regenRate} HP (Base: 1, Resting: ${game.currentAction === 'resting' ? 4 : 0})`);
-            }
-        if (game.currentAction === 'training') {
-            game.wielder.exp += 5;
-            if (game.wielder.exp >= 100 * game.wielder.level) {
-                game.wielder.exp -= 100 * game.wielder.level;
-                game.wielder.level++;
-                if (game.wielder.level === 5 && !game.wielder.hasFought) {
-                    game.statistics.hasPacifistLeveled = true;
-                }
-                game.wielder.statPoints += calculateStatPointsPerLevel();
-                applyLevelBonuses();
-                showLevelUpModal();
-            }
-        }
-        updateWielderStats();
-        saveGame();
-    }, 5000);
+
     showStory(true);
     setInterval(() => {
         game.statistics.totalPlayTime++;
@@ -46,6 +18,7 @@ async function initGame() {
         if (game.currentAction === 'autoExploring') {
             game.statistics.totalAutoExploreTime++;
         }
+        checkAchievements();
     }, 1000);
 }
 
